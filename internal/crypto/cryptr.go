a62975f8f41eb511cfbe276359f30b2d7d5bcfc3
Tomás Marques <tomascpmarques@gmail.com>
2024-11-08 09:54:44 +0000

    Refactor: Minor bug fixes, and message receiving handeling

--- internal/crypto/cryptr.go
+++ internal/crypto/cryptr.go
@@ -1,6 +1,8 @@
 /* Cryptr - A packge that supplies functions for algorithmicly sound encryption and decription, followed by secret generation. */
 package crypto
 
+import "crypto/rand"
+
 // Encrypt - Takes a byte array, and returns the encrypted version of the same array, using the given secret
 func Encrypt(content []byte, secret []byte) (cypher []byte) {
 	return
@@ -11,7 +13,14 @@ func Decrypt(cypher []byte, secret []byte) (content []byte) {
 	return
 }
 
-// GenerateSecret - Generates a secret based on the current time, and other factors
-func GenerateSecret() (secret []byte, err error) {
+// GenerateRawRandomBytes - Generates raw random bytes
+func GenerateRawRandomBytes() (secret []byte, err error) {
+	secret = make([]byte, 16*100)
+
+	_, err = rand.Read(secret)
+	if err != nil {
+		return nil, err
+	}
+
 	return
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -2,21 +2,55 @@ package server
 
 import (
 	"bufio"
+	"crypto/rand"
 	"errors"
+	"fmt"
 	"io"
 	"log"
 	"net"
 
-	"github.com/vmihailenco/msgpack/v5"
+	msgpack "github.com/vmihailenco/msgpack/v5"
 
+	crypto "github.com/TP-TS-Go/internal/crypto"
 	msgpacktyps "github.com/TP-TS-Go/internal/msgpack_typs"
 )
 
-func HandleNewConnection(con net.Conn) {
-	log.Println("New Connection!")
+type ServerState struct {
+	rooms   map[string]chan msgpacktyps.Message
+	clients map[string]string
+	secret  []byte
+}
+
+// RegisterNewClient - Returns a new cryptographicly seccure generated ID,
+// after adding the new client id to the server state, and raw material to build a secret.
+func (ss *ServerState) RegisterNewClient(clientAddr string) (string, string) {
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		log.Fatalf("erro ao tentar gerar id: %s", err.Error())
+	}
+
+	s, err := crypto.GenerateRawRandomBytes()
+	if err != nil {
+		log.Fatalf("erro ao tentar gerar secret raw material: %s", err.Error())
+	}
+
+	ss.clients[clientAddr] = fmt.Sprintf("%x", b)
+
+	return fmt.Sprintf("%x", b), fmt.Sprintf("%x", s)
+}
+
+func NewServerState() *ServerState {
+	return &ServerState{
+		rooms:   make(map[string]chan msgpacktyps.Message),
+		clients: make(map[string]string),
+	}
+}
+
+func HandleNewConnection(con net.Conn, serverState *ServerState) {
+	log.Printf("New Connection!")
 
 	buf := bufio.NewReader(con)
-
 	var decoder *msgpack.Decoder
 
 	for {
@@ -25,6 +59,7 @@ func HandleNewConnection(con net.Conn) {
 
 		// The loop pauses here waiting for the decoder to receive any new data
 		var msg msgpacktyps.Message
+
 		err := decoder.Decode(&msg)
 		log.Printf("DECODED SOME DATA")
 
@@ -37,7 +72,51 @@ func HandleNewConnection(con net.Conn) {
 			log.Printf("erro ao decodificar o MsgPack packet: %s", err.Error())
 		}
 
-		// Echo back the file contents
-		_, _ = con.Write([]byte("Received\n"))
+		// Deal with the message type and act accordingly
+		switch msg.Type {
+		case msgpacktyps.RequestId:
+
+			id, secretRawMaterial := serverState.RegisterNewClient(con.LocalAddr().String())
+			msg := msgpacktyps.NewMessage(
+				msgpacktyps.RequestIdResponse,
+				"",
+				[]byte(fmt.Sprintf("%s|%s", id, secretRawMaterial))...,
+			)
+
+			data, err := msgpack.Marshal(&msg)
+			if err != nil {
+				log.Fatalf("erro ao encodificar mensagem: %s", err.Error())
+			}
+
+			n, err := con.Write(append(data, 0x0a))
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Printf("Wrote: %d", n)
+
+		case msgpacktyps.SendContent:
+
+			msgg := msgpacktyps.NewMessage(
+				msgpacktyps.RequestIdResponse,
+				"",
+				[]byte("Received")...,
+			)
+
+			data, err := msgpack.Marshal(msgg)
+			if err != nil {
+				log.Fatalf("erro ao encodificar mensagem: %s", err.Error())
+			}
+
+			n, err := con.Write(append(data, 0x0a))
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			log.Printf("Wrote: %d", n)
+
+		default:
+			log.Println("tipo nao implementado, ignorar....")
+			continue
+		}
 	}
 }
